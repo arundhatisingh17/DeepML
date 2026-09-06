db01b69170fee342f30458950aabd33850dc6d69
Arundhati Singh <arundhatisingh171003@gmail.com>
2026-09-06 14:12:24 -0700

    Add matrix transpose

---
+++ transpose_matrix.py
@@ -0,0 +1,20 @@
+def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
+    """
+    Transpose a 2D matrix by swapping rows and columns.
+
+    Args:
+        a: A 2D matrix of shape (m, n)
+
+    Returns:
+        The transposed matrix of shape (n, m)
+    """
+
+    # to compute transpose of a matrix
+
+    mat = [[-1] * len(a) for _ in range(len(a[0]))]
+
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            mat[j][i] = a[i][j]
+
+    return mat
